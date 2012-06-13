0b023e077d2e0b7a41326da57748711d8d8237ed
Andy Green <andy.green@linaro.org>
2012-06-13 17:59:59 +0800

    OMAPDSS HDMI OMAP4 Update IRQ flags for the HPQ IRQ
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- drivers/video/omap2/dss/ti_hdmi_4xxx_ip.c
+++ drivers/video/omap2/dss/ti_hdmi_4xxx_ip.c
@@ -340,10 +340,11 @@ int ti_hdmi_4xxx_phy_enable(struct hdmi_ip_data *ip_data)
 	REG_FLD_MOD(phy_base, HDMI_TXPHY_PAD_CFG_CTRL, 0x1, 27, 27);
 
 	ip_data->irq = gpio_to_irq(ip_data->hpd_gpio);
+
 	r = request_threaded_irq(ip_data->irq,
-			NULL, hpd_irq_handler,
-			IRQF_DISABLED | IRQF_TRIGGER_RISING |
-			IRQF_TRIGGER_FALLING, "hpd", ip_data);
+				 NULL, hpd_irq_handler,
+				 IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING |
+				 IRQF_ONESHOT, "hpd", ip_data);
 	if (r) {
 		DSSERR("HPD IRQ request failed\n");
 		ip_data->irq = NO_IRQ;
